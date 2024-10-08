b70e916d2c3b3db23ff1afe3a92b4b4f6cfcea79
Dustin Peukert <dpeukert04@gmail.com>
2024-10-08 18:01:46 -0400

    write a test for Deck instance method #remove_card

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -61,4 +61,18 @@ describe Deck do
             expect(deck.percent_high_ranking).to eq(66.67)
         end
     end
+
+    describe '#remove_card' do
+        it 'will remove the first card in the deck' do
+            card1 = Card.new(:diamond, 'Queen', 12)
+            card2 = Card.new(:spade, '3', 3)
+            card3 = Card.new(:heart, 'Ace', 14)
+            cards = [card1, card2, card3]
+            deck = Deck.new(cards)
+
+            deck.remove_card
+
+            expect(deck.cards).to eq([card2, card3])
+        end
+    end
 end
