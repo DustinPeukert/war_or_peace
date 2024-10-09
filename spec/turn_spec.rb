5d58277c44aa8b0826d7e2007641279dc2d99205
Dustin Peukert <dpeukert04@gmail.com>
2024-10-08 21:41:09 -0400

    write tests for instance method #pile_cards for Turn class

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -180,4 +180,81 @@ describe Turn do
             expect(winner).to eq("No Winner")
         end
     end
+
+    describe '#pile_cards' do
+        it 'sends both players top cards to the spoils pile if turn type = :basic' do
+            card1 = Card.new(:heart, 'Jack', 11)
+            card2 = Card.new(:heart, '10', 10)
+            card3 = Card.new(:heart, '9', 9)
+            card4 = Card.new(:diamond, 'Jack', 11)
+            card5 = Card.new(:heart, '8', 8)
+            card6 = Card.new(:diamond, 'Queen', 12)
+            card7 = Card.new(:heart, '3', 3)
+            card8 = Card.new(:diamond, '2', 2)
+
+            deck1 = Deck.new([card1, card2, card5, card8])
+            deck2 = Deck.new([card3, card4, card6, card7])
+
+            player1 = Player.new("Megan", deck1)
+            player2 = Player.new("Aurora", deck2)
+
+            turn = Turn.new(player1, player2)
+
+            turn.pile_cards
+
+            expect(turn.spoils_of_war).to eq([card1, card3])
+            expect(player1.deck.cards).to eq([card2, card5, card8])
+            expect(player2.deck.cards).to eq([card4, card6, card7])
+        end
+
+        it 'sends both players top 3 cards to the spoils pile if turn type = :war' do
+            card1 = Card.new(:heart, 'Jack', 11)
+            card2 = Card.new(:heart, '10', 10)
+            card3 = Card.new(:heart, '9', 9)
+            card4 = Card.new(:diamond, 'Jack', 11)
+            card5 = Card.new(:heart, '8', 8)
+            card6 = Card.new(:diamond, 'Queen', 12)
+            card7 = Card.new(:heart, '3', 3)
+            card8 = Card.new(:diamond, '2', 2)
+
+            deck1 = Deck.new([card1, card2, card5, card8])
+            deck2 = Deck.new([card4, card3, card6, card7])
+
+            player1 = Player.new("Megan", deck1)
+            player2 = Player.new("Aurora", deck2)
+
+            turn = Turn.new(player1, player2)
+
+            turn.pile_cards
+
+            expect(turn.spoils_of_war).to eq([card1, card2, card5, card4, card3, card6])
+            expect(player1.deck.cards).to eq([card8])
+            expect(player2.deck.cards).to eq([card7])
+        end
+
+        it 'removes both players top 3 cards from game if turn type = :mutually_assured_destruction' do
+            card1 = Card.new(:heart, 'Jack', 11)
+            card2 = Card.new(:heart, '10', 10)
+            card3 = Card.new(:heart, '9', 9)
+            card4 = Card.new(:diamond, 'Jack', 11)
+            card5 = Card.new(:heart, '8', 8)
+            card6 = Card.new(:diamond, '8', 8)
+            card7 = Card.new(:heart, '3', 3)
+            card8 = Card.new(:diamond, '2', 2)
+
+            deck1 = Deck.new([card1, card2, card5, card8])
+            deck2 = Deck.new([card4, card3, card6, card7])
+                
+            player1 = Player.new("Megan", deck1)
+            player2 = Player.new("Aurora", deck2)
+
+            turn = Turn.new(player1, player2)
+
+            turn.pile_cards
+
+            expect(turn.spoils_of_war).to eq([])
+            expect(player1.deck.cards).to eq([card8])
+            expect(player2.deck.cards).to eq([card7])
+        end
+    end
 end
