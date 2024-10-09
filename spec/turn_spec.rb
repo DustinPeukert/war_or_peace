a7e6a712629c3eadd2974474f97586b93dd172a2
Dustin Peukert <dpeukert04@gmail.com>
2024-10-08 20:49:02 -0400

    create setup and write tests for instance method #type for Turn class

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -5,25 +5,26 @@ require './lib/player'
 require './lib/turn'
 
 describe Turn do
-    before(:each) do
-        @card1 = Card.new(:heart, 'Jack', 11)
-        @card2 = Card.new(:heart, '10', 10)
-        @card3 = Card.new(:heart, '9', 9)
-        @card4 = Card.new(:diamond, 'Jack', 11)
-        @card5 = Card.new(:heart, '8', 8)
-        @card6 = Card.new(:diamond, 'Queen', 12)
-        @card7 = Card.new(:heart, '3', 3)
-        @card8 = Card.new(:diamond, '2', 2)
-
-        @deck1 = Deck.new([@card1, @card2, @card5, @card8])
-        @deck2 = Deck.new([@card3, @card4, @card6, @card7])
-
-        @player1 = Player.new("Megan", @deck1)
-        @player2 = Player.new("Aurora", @deck2)
-
-        @turn = Turn.new(@player1, @player2)
-    end
     describe '#initialize' do
+        before(:each) do
+            @card1 = Card.new(:heart, 'Jack', 11)
+            @card2 = Card.new(:heart, '10', 10)
+            @card3 = Card.new(:heart, '9', 9)
+            @card4 = Card.new(:diamond, 'Jack', 11)
+            @card5 = Card.new(:heart, '8', 8)
+            @card6 = Card.new(:diamond, 'Queen', 12)
+            @card7 = Card.new(:heart, '3', 3)
+            @card8 = Card.new(:diamond, '2', 2)
+
+            @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+            @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+
+            @player1 = Player.new("Megan", @deck1)
+            @player2 = Player.new("Aurora", @deck2)
+
+            @turn = Turn.new(@player1, @player2)
+        end
+
         it 'is a turn' do
             expect(@turn).to be_a(Turn)
         end
@@ -35,6 +36,61 @@ describe Turn do
 
         it 'has no spoils of war by default' do
             expect(@turn.spoils_of_war).to eq([])
+        end
+    end
+
+    describe '#type' do
+        before(:each) do
+            @card1 = Card.new(:heart, 'Jack', 11)
+            @card2 = Card.new(:heart, '10', 10)
+            @card3 = Card.new(:heart, '9', 9)
+            @card4 = Card.new(:diamond, 'Jack', 11)
+            @card5 = Card.new(:heart, '8', 8)
+            @card6 = Card.new(:diamond, 'Queen', 12)
+            @card7 = Card.new(:heart, '3', 3)
+            @card8 = Card.new(:diamond, '2', 2)
+        end
+
+        describe ':basic turn' do
+            it 'returns :basic if both players first cards are different' do
+                deck1 = Deck.new([@card1, @card2, @card5, @card8])
+                deck2 = Deck.new([@card3, @card4, @card6, @card7])
+
+                player1 = Player.new("Megan", deck1)
+                player2 = Player.new("Aurora", deck2)
+
+                turn = Turn.new(player1, player2)
+
+                expect(turn.type).to eq(:basic)
+            end
+        end
+
+        describe ':war turn' do
+            it 'returns :war if both players first cards are equal' do
+                deck1 = Deck.new([@card1, @card2, @card5, @card8])
+                deck2 = Deck.new([@card4, @card3, @card6, @card7])
+
+                player1 = Player.new("Megan", deck1)
+                player2 = Player.new("Aurora", deck2)
+
+                turn = Turn.new(player1, player2)
+
+                expect(turn.type).to eq(:war)
+            end
+        end
+
+        describe ':mutually_assured_destruction turn' do
+            it 'returns :mutually_assured_destruction if both players 1st and 3rd cards match' do
+                deck1 = Deck.new([@card1, @card2, @card5, @card8])
+                deck2 = Deck.new([@card4, @card3, @card6, @card7])
+                
+                player1 = Player.new("Megan", deck1)
+                player2 = Player.new("Aurora", deck2)
+
+                turn = Turn.new(player1, player2)
+
+                expect(turn.type).to eq(:mutually_assured_destruction)
+            end
         end
     end
 end
