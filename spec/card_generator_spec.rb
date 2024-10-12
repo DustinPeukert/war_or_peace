39b285f71daaac77355becc67734bc1eedbc1b0a
Dustin Peukert <dpeukert04@gmail.com>
2024-10-12 16:38:42 -0400

    write test for CardGenerator class instance method #create_cards

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -19,4 +19,18 @@ describe CardGenerator do
       expect(card_generator.deck).to be_a(Deck)
     end
   end
+  
+  describe '#create_cards' do
+    it 'creates cards from a prebuilt text file' do
+      empty_deck = Deck.new([])
+      
+      card_generator = CardGenerator.new(empty_deck)
+      
+      expect(card_generator.deck.cards).to eq([])
+
+      card_generator.create_cards
+
+      expect(card_generator.deck.cards.count).to eq(4) # we have 4 cards listed in our testing txt file
+    end
+  end
 end
