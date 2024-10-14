050c9634c930409b4fd0ed4672b30f4517533f58
Dustin Peukert <dpeukert04@gmail.com>
2024-10-13 21:08:41 -0400

    write wrapper methods

--- lib/player.rb
+++ lib/player.rb
@@ -15,4 +15,12 @@ class Player
     def cards_amount
         @deck.cards.length # this is a violation of the demeter law, refactor this later after reading more
     end
+
+    def rank_of_card_at(index) # this method allows us to access the rank using just the player class, rather than chaining methods
+        @deck.rank_of_card_at(index) # avoids a violation of the law of Demeter
+    end
+
+    def remove_card
+        @deck.remove_card
+    end
 end
