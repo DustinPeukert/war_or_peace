924fac90f74603c73f5abb100862587f3f07dce0
Dustin Peukert <dpeukert04@gmail.com>
2024-10-14 16:43:47 -0400

    write code to make test more robust

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -18,13 +18,17 @@ describe CardGenerator do
     end
   end
   
-  describe '#create_cards' do
+  describe '#create_cards' do 
     it 'creates cards from a prebuilt text file' do
       card_generator = CardGenerator.new
       
       expect(card_generator.deck).to eq([])
 
-      card_generator.create_cards('./spec/cards_spec.txt')
+      card_generator.create_cards('./spec/fixtures/cards_spec.txt')
+
+      card_generator.deck.each do |card| # shows that each card is a Card object
+        expect(card).to be_a(Card)
+      end
 
       expect(card_generator.deck.count).to eq(4) # we have 4 cards listed in our testing txt file
     end
