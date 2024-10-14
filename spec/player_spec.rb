5db2d5f9a00668455f48b2f87c329bc2c3ce2399
Dustin Peukert <dpeukert04@gmail.com>
2024-10-14 17:55:25 -0400

    remove unnecessary line of code

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -39,7 +39,6 @@ describe Player do
             @player.deck.remove_card
             expect(@player.has_lost?).to be true
 
-            expect(@player.deck).to be_a(Deck)
             expect(@player.deck.cards).to be_empty
         end
     end
